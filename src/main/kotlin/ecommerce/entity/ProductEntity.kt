ab9accc9dba02131d1d5e2a93b971e46d8a0ba5f
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-30 19:37:30 +0200

    feat: create ProductEntity

---
+++ src/main/kotlin/ecommerce/entity/ProductEntity.kt
@@ -0,0 +1,30 @@
+package ecommerce.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Positive
+
+@Entity
+@Table(name = "product")
+class ProductEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @field:NotBlank
+    @Column(nullable = false, unique = true)
+    val name: String,
+    @field:Positive
+    @Column(nullable = false)
+    val price: Double,
+    @field:Pattern(
+        regexp = "https?://.*",
+        message = "Image URL must start with http:// or https://",
+    )
+    @Column(nullable = false, name = "image_url")
+    val imageUrl: String,
+)
