06c36cf428a27e01bc1b77d89c72ca8bd2dcdec5
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-30 19:35:11 +0200

    chore: add dependencies for JPA implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "camp.nextstep.edu"
@@ -20,7 +21,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
