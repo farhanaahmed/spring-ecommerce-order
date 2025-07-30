4bb939d9277f133f75bba61355e6793a36cf2aee
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-30 19:42:04 +0200

    feat: create MemberEntity

---
+++ src/main/kotlin/ecommerce/entity/MemberEntity.kt
@@ -0,0 +1,23 @@
+package ecommerce.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "member")
+class MemberEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(nullable = false, unique = true)
+    val email: String,
+    @Column(nullable = false)
+    val password: String,
+    @Column(nullable = false)
+    val role: String,
+    @Column(nullable = false)
+    val name: String,
+)
