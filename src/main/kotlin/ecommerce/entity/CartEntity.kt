4dc392ae7eab6bac6b7334803b5ed6ef0556b55a
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-31 18:52:37 +0200

    feat: implement Cart entity

--- src/main/kotlin/ecommerce/entity/CartEntity.kt
+++ src/main/kotlin/ecommerce/entity/CartEntity.kt
@@ -1,23 +1,24 @@
 package ecommerce.entity
 
-import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
+import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
-import jakarta.validation.constraints.Positive
 
 @Entity
 @Table(name = "cart")
 class CartEntity(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    @Column(nullable = false)
-    val memberId: Long,
-    @Column(nullable = false)
-    val productId: Long,
-    @field:Positive
-    @Column(nullable = false)
-    val quantity: Int,
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    val memberEntity: MemberEntity? = null,
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_item_id", nullable = false)
+    var CartItemEntities: MutableList<CartItemEntity>? = mutableListOf(),
 )
