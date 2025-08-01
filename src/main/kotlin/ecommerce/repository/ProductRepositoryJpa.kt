8b5a60316fef62063813debeffbeb8f340fc596c
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-08-01 17:51:08 +0200

    feat: implement pagination

--- src/main/kotlin/ecommerce/repository/ProductRepositoryJpa.kt
+++ src/main/kotlin/ecommerce/repository/ProductRepositoryJpa.kt
@@ -1,10 +1,25 @@
 package ecommerce.repository
 
 import ecommerce.entity.ProductEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ProductRepositoryJpa : JpaRepository<ProductEntity, Long> {
     fun findByName(name: String): ProductEntity?
 
     fun existsByName(name: String): Boolean
+
+    fun findAllByPrice(
+        price: Double,
+        pageable: Pageable,
+    ): Page<ProductEntity>
+
+    override fun findAll(pageable: Pageable): Page<ProductEntity>
+
+    fun findByPriceGreaterThan(
+        price: Double,
+        pageable: Pageable,
+    ): Slice<ProductEntity>
 }
--- src/main/kotlin/ecommerce/service/ProductService.kt
+++ src/main/kotlin/ecommerce/service/ProductService.kt
@@ -1,14 +1,21 @@
 package ecommerce.service
 
 import ecommerce.dto.ProductRequest
+import ecommerce.entity.ProductEntity
 import ecommerce.model.Product
+import ecommerce.repository.ProductRepositoryJpa
 import ecommerce.repository.ProductStore
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Slice
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 
 @Service
 class ProductService(
     @Qualifier("jdbcProductStore") private val productRepository: ProductStore,
+    private val productRepositoryJpa: ProductRepositoryJpa,
 ) {
     fun createProduct(productRequest: ProductRequest): Product {
         if (productRepository.existsByName(productRequest.name)) {
@@ -21,5 +28,33 @@ class ProductService(
                 imageUrl = productRequest.imageUrl,
             )
         return productRepository.save(product)
+    }
+
+    fun getAllProducts(
+        page: Int,
+        size: Int,
+        sortBy: String = "name",
+        direction: Sort.Direction = Sort.Direction.ASC,
+    ): Page<ProductEntity> {
+        val pageable = PageRequest.of(page, size, Sort.by(direction, sortBy))
+        return productRepositoryJpa.findAll(pageable)
+    }
+
+    fun getProductsByPrice(
+        price: Double,
+        page: Int,
+        size: Int,
+    ): Page<ProductEntity> {
+        val pageable = PageRequest.of(page, size)
+        return productRepositoryJpa.findAllByPrice(price, pageable)
+    }
+
+    fun getPriceSlice(
+        price: Double,
+        page: Int,
+        size: Int,
+    ): Slice<ProductEntity> {
+        val pageable = PageRequest.of(page, size)
+        return productRepositoryJpa.findByPriceGreaterThan(price, pageable)
     }
 }
