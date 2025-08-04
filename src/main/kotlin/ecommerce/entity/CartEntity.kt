1adfb8367a7dedd39c578d25279bec956207d5a8
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-08-04 13:28:29 +0200

    refactor: change Cart Entity

--- src/main/kotlin/ecommerce/entity/CartEntity.kt
+++ src/main/kotlin/ecommerce/entity/CartEntity.kt
@@ -2,12 +2,9 @@ package ecommerce.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import java.time.LocalDateTime
 
@@ -16,9 +13,8 @@ import java.time.LocalDateTime
 class CartEntity(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "member_id", nullable = false)
-    val memberEntity: MemberEntity? = null,
+    @Column(name = "member_id", nullable = false)
+    val memberId: Long,
     @Column(name = "created_at", nullable = false)
     val createdAt: LocalDateTime = LocalDateTime.now(),
 )
